9120158c5ab93e604265f1399070a6303ad17472
8ark-dev <sorlti6952@gmail.com>
2024-05-15 03:10:49 +0900

    design: 환율 input 디자인 구성

--- src/views/ExchangeView.vue
+++ src/views/ExchangeView.vue
@@ -9,11 +9,13 @@
     </div>
 
     <!-- input -->
-    <div class="bg-theme p-2 rounded-lg">
-      <div class="w-[500px] flex items-center py-4">
+    <div
+      class="w-[600px] bg-white border border-theme rounded-lg flex justify-between items-center"
+    >
+      <div class="flex items-center p-4 bg-theme rounded-l-lg">
         <select
           v-model="FromselectedExchange"
-          class="text-white font-bold focus:outline-none appearance-none bg-transparent text-center"
+          class="text-white px-2 font-bold focus:outline-none appearance-none bg-transparent text-center"
         >
           <option
             v-for="exchange in dummyExchange"
@@ -24,14 +26,15 @@
             {{ exchange.name }}
           </option>
         </select>
-        <input
-          type="text"
-          placeholder="0"
-          class="text-theme flex-1 focus:outline-none text-end pr-4"
-          v-model="FromNum"
-          @input="calExchange"
-        />
       </div>
+
+      <input
+        type="text"
+        :placeholder="FromselectedExchange.symbol"
+        class="text-theme flex-1 focus:outline-none text-end pr-4"
+        v-model="FromNum"
+        @input="calExchange"
+      />
     </div>
 
     <!-- result -->
@@ -39,9 +42,9 @@
       v-if="FromNum && ToNum"
       class="text-theme gap-y-4 text-4xl font-bold flex flex-col justify-center items-center"
     >
-      <p>{{ FromNum }}$</p>
+      <p>{{ FromNum }} 원</p>
       <p>=</p>
-      <p>{{ ToNum }}{{ FromselectedExchange.symbol }}</p>
+      <p>{{ ToNum }} {{ FromselectedExchange.symbol }}</p>
     </div>
   </div>
 </template>
@@ -51,13 +54,18 @@ import { ref, watch } from 'vue';
 const FromNum = ref('');
 const ToNum = ref('');
 
-const FromselectedExchange = ref({ name: '미국 달러', rate: 1, symbol: '$' });
+const FromselectedExchange = ref({
+  name: '대한민국 원',
+  rate: 1,
+  symbol: '원',
+});
 
 const dummyExchange = ref([
-  { name: '미국 달러', rate: 1, symbol: '$' },
-  { name: '대한민국 원', rate: 1130, symbol: '₩' },
-  { name: '유럽 유로', rate: 0.84, symbol: '€' },
-  { name: '일본 엔', rate: 110, symbol: '¥' },
+  { name: '대한민국 원', rate: 1, symbol: '원' },
+  { name: '미국 달러', rate: 0.00073, symbol: '달러' },
+  { name: '유럽 유로', rate: 0.00068, symbol: '유로' },
+  { name: '일본 엔', rate: 0.11, symbol: '엔' },
+  { name: '중국 위안', rate: 0.0053, symbol: '위안' },
 ]);
 
 watch(FromselectedExchange, () => {
