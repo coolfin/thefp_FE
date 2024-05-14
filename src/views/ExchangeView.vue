7265bd06f04cd3a7a1a5ce673e9c2077f7385f64
8ark-dev <sorlti6952@gmail.com>
2024-05-15 02:24:40 +0900

    feat: nav 구성

---
+++ src/views/ExchangeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>ex</h1>
+  </div>
+</template>
+<script setup></script>
+<style scoped></style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <nav class="p-4 font-bold bg-red-200 flex items-center gap-x-8">
+      <!-- logo -->
+      <div
+        class="flex items-center gap-x-2 font-bold cursor-pointer"
+        @click="moveInfo"
+      >
+        <img src="../assets/logo.png" alt="logo" class="w-8 h-8" />
+        <h1>THE FP</h1>
+      </div>
+
+      <RouterLink :to="{ name: 'info' }">상품정보</RouterLink>
+      <RouterLink :to="{ name: 'exchange' }">환율</RouterLink>
+
+      <div class="flex-1 text-sm bg-blue-200 flex items-center justify-end">
+        <span class="font-bold">admin</span>
+        님
+      </div>
+    </nav>
+
+    <RouterView />
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const moveInfo = () => {
+  router.push('/main/info');
+};
+</script>
+<style scoped></style>
