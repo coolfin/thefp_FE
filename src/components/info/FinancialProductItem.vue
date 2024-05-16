272f402b9b8ad88534b7590e7658edefaae75d7f
coolfin <sorlti6952@gmail.com>
2024-05-16 10:04:14 +0900

    feat: 상품정보 페이지 구현 - 더미데이터 추가 예정

--- src/components/info/FinancialProductItem.vue
+++ src/components/info/FinancialProductItem.vue
@@ -1,15 +1,47 @@
 <template>
-  <div class="flex items-center px-2 py-4">
-    <div class="w-1/12">이미지</div>
+  <div class="relative">
+    <div
+      v-if="item === 2"
+      class="absolute top-0 left-0 z-10 w-full h-full bg-opacity-80 bg-[#333] flex items-center justify-center rounded-lg"
+    >
+      <span class="text-2xl font-bold text-[#AEF6C7]">종료된 상품입니다.</span>
+    </div>
+    <div
+      @click="clickFinancialProduct"
+      class="flex items-center px-2 py-4 hover:bg-[#eee] rounded-lg cursor-pointer transition duration-300 ease-in-out"
+    >
+      <div class="flex items-center w-2/12 gap-x-4">
+        <img
+          class="w-10 rounded-lg"
+          src="@/assets/icons/banks/nh-logo.svg"
+          alt="농협은행"
+        />
+        <span>농협은행</span>
+      </div>
 
-    <div class="w-1/12">타입</div>
+      <div class="w-1/12">
+        <div
+          class="px-3 py-1 text-sm border rounded-full text-theme border-theme w-fit"
+        >
+          적금
+        </div>
+      </div>
 
-    <div class="w-3/12 truncate">상품명</div>
+      <div class="w-3/12 truncate">상품명입니다</div>
 
-    <div class="flex-1">금리</div>
+      <div class="flex-1">2.1%</div>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const clickFinancialProduct = () => {
+  alert("죄송합니다. 상품 상세 페이지는 준비 중입니다.🤐");
+};
+
+defineProps({
+  item: Object,
+});
+</script>
 
 <style scoped></style>
--- src/components/info/FinancialProducts.vue
+++ src/components/info/FinancialProducts.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="bg-red-200">
+  <div>
     <!-- header -->
     <div class="flex items-center px-2 py-4 font-bold">
-      <span class="w-1/12"> 금융회사 </span>
+      <span class="w-2/12"> 금융회사 </span>
 
       <span class="w-1/12">타입</span>
 
       <span class="w-3/12">상품명</span>
 
-      <span class="flex-1">금리</span>
+      <span class="flex-1 text-start">금리</span>
     </div>
 
     <!-- data -->
-    <FinancialProductItem v-for="item in 3" :key="item" />
+    <div class="flex flex-col gap-y-1">
+      <FinancialProductItem v-for="item in 3" :key="item" :item="item" />
+    </div>
   </div>
 </template>
 <script setup>
