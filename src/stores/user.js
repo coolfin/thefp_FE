93b2358d2503a5d42352296ba46d2b957089a5b3
coolfin <sorlti6952@gmail.com>
2024-05-16 16:07:00 +0900

    feat: 로그인 기능 구현 - 더미데이터 활용 - try catch를 활용하여 에러텍스트 출력

---
+++ src/stores/user.js
@@ -0,0 +1,43 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useUserStore = defineStore("user", () => {
+  const dummyUser = {
+    id: 1,
+    name: "홍길동",
+    birth: "1990-01-01",
+    email: "test@test.com",
+    password: "1234",
+  };
+
+  const users = ref([dummyUser]);
+  const loginUser = ref(null);
+
+  const login = (email, password) => {
+    const user = users.value.find((user) => user.email === email);
+    if (!user) {
+      throw new Error("가입되지 않은 이메일입니다.");
+    }
+    if (user.password !== password) {
+      throw new Error("비밀번호가 일치하지 않습니다.");
+    }
+    loginUser.value = user;
+  };
+
+  const signup = (user) => {
+    users.value.map((userData) => {
+      if (userData.email === user.email) {
+        throw new Error("이미 가입된 이메일입니다.");
+      }
+    });
+    users.value.push(user);
+    console.log("서버측: ", users.value);
+  };
+
+  return {
+    users,
+    loginUser,
+    login,
+    signup,
+  };
+});
