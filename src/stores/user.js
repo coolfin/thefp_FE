d56310c42f62747a381d65dd32fce54fb24b0f9b
coolfin <sorlti6952@gmail.com>
2024-05-16 16:36:00 +0900

    feat: 회원가입 구현

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,43 +1,49 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-export const useUserStore = defineStore("user", () => {
-  const dummyUser = {
-    id: 1,
-    name: "홍길동",
-    birth: "1990-01-01",
-    email: "test@test.com",
-    password: "1234",
-  };
+export const useUserStore = defineStore(
+  "user",
+  () => {
+    const dummyUser = {
+      id: 1,
+      name: "홍길동",
+      birth: "1990-01-01",
+      email: "test@test.com",
+      password: "1234",
+    };
 
-  const users = ref([dummyUser]);
-  const loginUser = ref(null);
+    const users = ref([dummyUser]);
+    const loginUser = ref(null);
 
-  const login = (email, password) => {
-    const user = users.value.find((user) => user.email === email);
-    if (!user) {
-      throw new Error("가입되지 않은 이메일입니다.");
-    }
-    if (user.password !== password) {
-      throw new Error("비밀번호가 일치하지 않습니다.");
-    }
-    loginUser.value = user;
-  };
-
-  const signup = (user) => {
-    users.value.map((userData) => {
-      if (userData.email === user.email) {
-        throw new Error("이미 가입된 이메일입니다.");
+    const login = (email, password) => {
+      const user = users.value.find((user) => user.email === email);
+      if (!user) {
+        throw new Error("가입되지 않은 이메일입니다.");
       }
-    });
-    users.value.push(user);
-    console.log("서버측: ", users.value);
-  };
+      if (user.password !== password) {
+        throw new Error("비밀번호가 일치하지 않습니다.");
+      }
+      loginUser.value = user;
+      alert(loginUser.value.name + "님 환영합니다.");
+    };
 
-  return {
-    users,
-    loginUser,
-    login,
-    signup,
-  };
-});
+    const signup = (user) => {
+      users.value.map((userData) => {
+        if (userData.email === user.email) {
+          throw new Error("이미 가입된 이메일입니다.");
+        }
+      });
+      users.value.push({ id: users.value.length + 1, ...user });
+    };
+
+    return {
+      users,
+      loginUser,
+      login,
+      signup,
+    };
+  },
+  {
+    persist: true,
+  }
+);
