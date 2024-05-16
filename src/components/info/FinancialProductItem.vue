25534af716e713e51f5732f50fa8d16d9deb131b
coolfin <sorlti6952@gmail.com>
2024-05-16 09:33:56 +0900

    design: 금융상품 테이블 헤더 디자인

---
+++ src/components/info/FinancialProductItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex items-center px-2 py-4">
+    <div class="w-1/12">이미지</div>
+
+    <div class="w-1/12">타입</div>
+
+    <div class="w-3/12 truncate">상품명</div>
+
+    <div class="flex-1">금리</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- src/components/info/FinancialProducts.vue
+++ src/components/info/FinancialProducts.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="bg-red-200">table</div>
+  <div class="bg-red-200">
+    <!-- header -->
+    <div class="flex items-center px-2 py-4 font-bold">
+      <span class="w-1/12"> 금융회사 </span>
+
+      <span class="w-1/12">타입</span>
+
+      <span class="w-3/12">상품명</span>
+
+      <span class="flex-1">금리</span>
+    </div>
+
+    <!-- data -->
+    <FinancialProductItem v-for="item in 3" :key="item" />
+  </div>
 </template>
-<script setup></script>
+<script setup>
+import FinancialProductItem from "@/components/info/FinancialProductItem.vue";
+</script>
 <style scoped></style>
