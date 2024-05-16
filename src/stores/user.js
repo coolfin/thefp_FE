d8107055eca6d8fb66277efcbcb543eee1fd6a66
coolfin <sorlti6952@gmail.com>
2024-05-16 19:35:54 +0900

    feat: 비밀번호 찾기 구현

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,9 +1,10 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
 export const useUserStore = defineStore(
   "user",
   () => {
+    //state
     const dummyUser = {
       id: 1,
       name: "홍길동",
@@ -13,8 +14,16 @@ export const useUserStore = defineStore(
     };
 
     const users = ref([dummyUser]);
-    const loginUser = ref(null);
 
+    const loginUser = ref(null);
+    const findUser = ref(null);
+
+    //getter
+    const isFoundUser = computed(() => {
+      return findUser.value === null;
+    });
+
+    //action
     const login = (email, password) => {
       const user = users.value.find((user) => user.email === email);
       if (!user) {
@@ -36,11 +45,24 @@ export const useUserStore = defineStore(
       users.value.push({ id: users.value.length + 1, ...user });
     };
 
+    const findEmail = (email) => {
+      const user = users.value.find((user) => user.email === email);
+      if (!user) {
+        throw new Error("가입되지 않은 이메일입니다.");
+      }
+
+      findUser.value = user;
+      alert(findUser.value.email + "로 이메일을 전송하였습니다.");
+    };
+
     return {
       users,
       loginUser,
+      findUser,
+      isFoundUser,
       login,
       signup,
+      findEmail,
     };
   },
   {
