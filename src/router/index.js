4eea011b474f94178a0cfad96d4d6e8b129a955c
coolfin <sorlti6952@gmail.com>
2024-05-16 16:52:12 +0900

    feat: router protect 기능 추가

--- src/router/index.js
+++ src/router/index.js
@@ -14,33 +14,45 @@ const router = createRouter({
       component: () => import("@/views/SignupView.vue"),
     },
     {
-      path: '/main',
-      name: 'main',
-      component: () => import('@/views/MainView.vue'),
-      redirect: { name: 'info' },
+      path: "/main",
+      name: "main",
+      component: () => import("@/views/MainView.vue"),
+      redirect: { name: "info" },
       children: [
         {
-          path: 'info',
-          name: 'info',
-          component: () => import('@/views/InfoView.vue')
+          path: "info",
+          name: "info",
+          component: () => import("@/views/InfoView.vue"),
         },
         {
-          path: 'exchange',
-          name: 'exchange',
-          component: () => import('@/views/ExchangeView.vue')
-        }
-      ]
+          path: "exchange",
+          name: "exchange",
+          component: () => import("@/views/ExchangeView.vue"),
+        },
+      ],
     },
     {
-      path: '/404',
-      name: '404',
-      component: () => import('@/views/NotFoundView.vue')
+      path: "/404",
+      name: "404",
+      component: () => import("@/views/NotFoundView.vue"),
     },
     {
-      path: '/:pathMatch(.*)*',
-      redirect: { name: '404' }
-    }
+      path: "/:pathMatch(.*)*",
+      redirect: { name: "404" },
+    },
   ],
 });
 
+import { useUserStore } from "@/stores/user";
+router.beforeEach((to, from, next) => {
+  const store = useUserStore();
+  if (!store.loginUser && to.name !== "home" && to.name !== "signup") {
+    alert("로그인이 필요합니다. 로그인 페이지로 이동합니다.");
+    next({ name: "home" });
+  } else if (store.loginUser && (to.name === "home" || to.name === "signup")) {
+    next({ name: "info" });
+  } else {
+    next();
+  }
+});
 export default router;
