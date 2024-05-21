8eed67cbf86e886b49860746aea80807832dd7a5
coolfin <sorlti6952@gmail.com>
2024-05-21 16:30:29 +0900

    feat: gpt api 연결

--- src/stores/gpt.js
+++ src/stores/gpt.js
@@ -1,5 +1,6 @@
 import { defineStore } from "pinia";
 import { ref, computed } from "vue";
+import OpenAI from "openai";
 
 export const useGptStore = defineStore(
   "gpt",
@@ -8,22 +9,48 @@ export const useGptStore = defineStore(
     const isLoadingGpt = ref(false);
 
     const getChat = computed(() => chat.value);
+    const getIsLoadingGpt = computed(() => isLoadingGpt.value);
 
-    const sendMessage = (text) => {
+    const sendMessage = async (text) => {
       chat.value.push({ text, type: "user" });
 
+      const openai = new OpenAI({
+        apiKey: import.meta.env.VITE_GPT_API_KEY,
+        dangerouslyAllowBrowser: true,
+      });
       //axios로 gpt-3 api 호출하는 코드 추가
-      setTimeout(() => {
+      try {
         isLoadingGpt.value = true;
-        chat.value.push({ text: "안녕하세요", type: "oper" });
-      }, 2000);
+        const res = await openai.chat.completions.create({
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "system",
+              content:
+                "You are an expert in financial knowledge, including savings accounts, fixed deposits, and personal loans.",
+            },
+            {
+              role: "user",
+              content: text,
+            },
+          ],
+          temperature: 0.7,
+          top_p: 0.9,
+        });
 
+        chat.value.push({ text: res.choices[0].message.content, type: "oper" });
+
+        //아래로 스크롤
+      } catch (error) {
+        console.error("err", error);
+      }
       isLoadingGpt.value = false;
     };
     return {
       chat,
       isLoadingGpt,
       getChat,
+      getIsLoadingGpt,
       sendMessage,
     };
   },
